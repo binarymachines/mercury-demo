fa58ae309988a2de28c26b64cbb122924c2a083e
Dexter Taylor <binarymachineshop@gmail.com>
2020-02-29 16:56:24 -0500

    adding files for third module

---
+++ xfile_3/test_datasources.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+
+class TestDatasource(object):
+    def __init__(self, service_object_registry):
+        self.services = service_object_registry
+
+
+    def lookup_longevity(self, target_field_name, source_record, field_value_map):
+        return '###'
+
